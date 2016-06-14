a3454eb3590b6d800806c58898cd093647b88e76
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2016-06-14 11:48:15 +0100

    save work

---
+++ src/Config.Net.Tests/SettingTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Config.Net.Tests
+{
+   [TestFixture]
+   public class SettingTest
+   {
+      public void 
+   }
+}
--- src/Config.Net/Setting.cs
+++ src/Config.Net/Setting.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Config.Net
 {
