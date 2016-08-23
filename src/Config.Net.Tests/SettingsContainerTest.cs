e726425f85b3a5562de2e37006139c97d53ba2cc
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2016-08-23 15:02:24 +0100

    just save work

--- src/Config.Net.Tests/SettingsContainerTest.cs
+++ src/Config.Net.Tests/SettingsContainerTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Config.Net;
+using Config.Net.Stores;
 using NUnit.Framework;
 
 namespace Config.Net.Tests
@@ -11,10 +12,21 @@ namespace Config.Net.Tests
    [TestFixture]
    public class SettingsContainerTest
    {
+      private IConfigStore _store;
+
+      [SetUp]
+      public void SetUp()
+      {
+         _store = new InMemoryConfigStore();
+      }
+
       class MyContainer : SettingsContainer
       {
-         public MyContainer() : base("MyApp")
+         private IConfigStore _store;
+
+         public MyContainer(IConfigStore store) : base("MyApp")
          {
+            _store = store;
          }
 
          public int Timeout { get; set; }
@@ -24,14 +36,14 @@ namespace Config.Net.Tests
 
          protected override void OnConfigure(IConfigConfiguration configuration)
          {
-            configuration.UseEnvironmentVariables();
+            configuration.AddStore(_store);
          }
       }
 
       [Test]
       public void Smoke()
       {
-         var c = new MyContainer();
+         var c = new MyContainer(_store);
 
          int timeout = c.Timeout;
       }
