d6d738b38257ebaed1de50d1f91b74b01e53e7a6
Ivan Gavryliuk <ivan@isolineltd.com>
2016-09-30 16:00:03 +0100

    updated packages and added KeyVault package

---
+++ src/Config.Net.Azure/AzureKeyVaultConfigStore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+
+namespace Config.Net.Azure
+{
+   class AzureKeyVaultConfigStore : IConfigStore
+   {
+      private KeyVaultClient _vaultClient;
+
+      public AzureKeyVaultConfigStore()
+      {
+
+      }
+
+      public bool CanRead
+      {
+         get
+         {
+            throw new NotImplementedException();
+         }
+      }
+
+      public bool CanWrite
+      {
+         get
+         {
+            throw new NotImplementedException();
+         }
+      }
+
+      public string Name
+      {
+         get
+         {
+            throw new NotImplementedException();
+         }
+      }
+
+      public void Dispose()
+      {
+         throw new NotImplementedException();
+      }
+
+      public string Read(string key)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Write(string key, string value)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
