9338d50839f1c11448aeba2ff4b8df3fd966aa3e
Ivan Gavryliuk <ivan@isolineltd.com>
2017-07-05 22:18:37 +0100

    migrating to appveyor

---
+++ appveyor.ps1
@@ -0,0 +1,82 @@
+$gv = $env:APPVEYOR_BUILD_VERSION
+if($gv -eq $null)
+{
+   $gv = "3.2.0"
+}
+$vt = @{
+   "Config.Net.Integration.Storage.Net.csproj" = "1.0.0-alpha-1";
+}
+
+$Copyright = "Copyright (c) 2015-2017 by Ivan Gavryliuk"
+$PackageIconUrl = "http://i.isolineltd.com/nuget/config.net.png"
+$PackageProjectUrl = "https://github.com/aloneguid/config"
+$RepositoryUrl = "https://github.com/aloneguid/config"
+$Authors = "Ivan Gavryliuk (@aloneguid)"
+$PackageLicenseUrl = "https://github.com/aloneguid/config/blob/master/LICENSE"
+$RepositoryType = "GitHub"
+
+$SlnPath = "src\Config.Net.sln"
+
+function Update-ProjectVersion($File)
+{
+   $v = $vt.($File.Name)
+   if($v -eq $null) { $v = $gv }
+
+   $xml = [xml](Get-Content $File.FullName)
+
+   if($xml.Project.PropertyGroup.Count -eq $null)
+   {
+      $pg = $xml.Project.PropertyGroup
+   }
+   else
+   {
+      $pg = $xml.Project.PropertyGroup[0]
+   }
+
+   $parts = $v -split "\."
+   $bv = $parts[2]
+   if($bv.Contains("-")) { $bv = $bv.Substring(0, $bv.IndexOf("-"))}
+   $fv = "{0}.{1}.{2}.0" -f $parts[0], $parts[1], $bv
+   $av = "{0}.0.0.0" -f $parts[0]
+   $pv = $v
+
+   $pg.Version = $pv
+   $pg.FileVersion = $fv
+   $pg.AssemblyVersion = $av
+
+   Write-Host "$($File.Name) => fv: $fv, av: $av, pkg: $pv"
+
+   $pg.Copyright = $Copyright
+   $pg.PackageIconUrl = $PackageIconUrl
+   $pg.PackageProjectUrl = $PackageProjectUrl
+   $pg.RepositoryUrl = $RepositoryUrl
+   $pg.Authors = $Authors
+   $pg.PackageLicenseUrl = $PackageLicenseUrl
+   $pg.RepositoryType = $RepositoryType
+
+   $xml.Save($File.FullName)
+}
+
+function Exec($Command, [switch]$ContinueOnError)
+{
+   Invoke-Expression $Command
+   if($LASTEXITCODE -ne 0)
+   {
+      Write-Error "command failed (error code: $LASTEXITCODE)"
+
+      if(-not $ContinueOnError.IsPresent)
+      {
+         exit 1
+      }
+   }
+}
+
+# Update versioning information
+Get-ChildItem *.csproj -Recurse | Where-Object {-not($_.Name -like "*test*")} | % {
+   Update-ProjectVersion $_
+}
+
+# Restore packages
+Exec "dotnet restore $SlnPath"
+
+Write-Host "build succeeded."
---
+++ src/Config.Net.Integration.Storage.Net/TableConfigStore.cs
@@ -0,0 +1,56 @@
+﻿using Storage.Net.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Config.Net.Integration.Storage.Net
+{
+   class TableConfigStore : IConfigStore
+   {
+      private readonly ITableStorage _tableStorage;
+      private readonly string _tableName;
+      private readonly string _partitionKey;
+
+      public TableConfigStore(ITableStorage tableStorage, string tableName, string partitionKey)
+      {
+         _tableStorage = tableStorage;
+         _tableName = tableName;
+         _partitionKey = partitionKey;
+      }
+
+      public string Name => "Storage.Net Tables";
+
+      public bool CanRead => true;
+
+      public bool CanWrite => true;
+
+      public void Dispose()
+      {
+         _tableStorage.Dispose();
+      }
+
+      public string Read(string key)
+      {
+         TableRow row = _tableStorage.Get(_tableName, _partitionKey, key);
+         if (row == null) return null;
+
+         return row["value"];
+      }
+
+      public void Write(string key, string value)
+      {
+         var rowId = new TableRowId(_partitionKey, key);
+
+         if (value == null)
+         {
+            _tableStorage.Delete(_tableName, rowId);
+         }
+         else
+         {
+            var row = new TableRow(rowId);
+            row["value"] = value;
+            _tableStorage.InsertOrReplace(_tableName, row);
+         }
+      }
+   }
+}
