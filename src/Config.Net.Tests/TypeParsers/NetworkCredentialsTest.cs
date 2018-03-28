41783f4b5af0f5bb75b25b52549c5b9e94c6a518
Richard Conway <richard@elastacloud.com>
2018-03-28 23:53:10 +0100

    test for NetworkCredentials

---
+++ src/Config.Net.Tests/TypeParsers/NetworkCredentialsTest.cs
@@ -0,0 +1,26 @@
+﻿using Config.Net.TypeParsers;
+using System.Globalization;
+using System.Net;
+using System.Threading;
+using Xunit;
+
+namespace Config.Net.Tests.TypeParsers
+{
+   public class NetworkCredentialsParserTest
+   {
+      private static readonly ITypeParser TypeParser = new NetworkCredentialParser();
+
+      [Theory]
+      [InlineData("user:pass@domain")]
+      public void ToNetworkCredential_WhenInputIsValid_ReturnValidStringCredentials(string rawValue)
+      {
+         object creds = null;
+         Assert.True(TypeParser.TryParse(rawValue, typeof(NetworkCredential), out creds));
+         var networkCredentials = (NetworkCredential) creds;
+
+         Assert.Equal("user", networkCredentials.UserName);
+         Assert.Equal("pass", networkCredentials.Password);
+         Assert.Equal("domain", networkCredentials.Domain);
+      }
+   }
+}
