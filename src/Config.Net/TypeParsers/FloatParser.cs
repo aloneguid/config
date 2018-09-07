a53ff4545b27f4071b05502ccaedff07fcfad4a1
Zhou Bo <mikefoxnobel@gmail.com>
2018-09-07 11:54:35 +0800

    More basic value type parser

---
+++ src/Config.Net/TypeParsers/FloatParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Config.Net.TypeParsers
+{
+   class FloatParser : ITypeParser
+   {
+      public IEnumerable<Type> SupportedTypes => new[] { typeof(float) };
+
+      public bool TryParse(string value, Type t, out object result)
+      {
+         float dr;
+         bool parsed = float.TryParse(value, NumberStyles.Float, TypeParserSettings.DefaultCulture, out dr);
+         result = dr;
+         return parsed;
+
+      }
+
+      public string ToRawString(object value)
+      {
+         return ((float)value).ToString(TypeParserSettings.DefaultNumericFormat, TypeParserSettings.DefaultCulture);
+      }
+   }
+}
---
+++ src/Config.Net/TypeParsers/ShortParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Config.Net.TypeParsers
+{
+   class ShortParser : ITypeParser
+   {
+      public IEnumerable<Type> SupportedTypes => new[] { typeof(int) };
+
+      public bool TryParse(string value, Type t, out object result)
+      {
+         short ir;
+         bool parsed = short.TryParse(value, NumberStyles.Integer, TypeParserSettings.DefaultCulture, out ir);
+         result = ir;
+         return parsed;
+      }
+
+      public string ToRawString(object value)
+      {
+         return ((short)value).ToString(TypeParserSettings.DefaultNumericFormat, TypeParserSettings.DefaultCulture);
+      }
+   }
+}
