c744bdd0c684a33731d65ebeaa635d5f878768f2
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2019-02-11 14:05:22 +0000

    add overload for kv store

--- src/Impl/Azure/Config.Net.Azure.KeyVault/ConfigurationExtensions.cs
+++ src/Impl/Azure/Config.Net.Azure.KeyVault/ConfigurationExtensions.cs
@@ -15,6 +15,11 @@ namespace Config.Net
       public static ConfigurationBuilder<TInterface> UseAzureKeyVaultWithManagedIdentity<TInterface>(
          this ConfigurationBuilder<TInterface> builder, Uri vaultUri) where TInterface : class
       {
+         if (vaultUri == null)
+         {
+            throw new ArgumentNullException(nameof(vaultUri));
+         }
+
          builder.UseConfigStore(AzureKeyVaultConfigStore.CreateWithManagedIdentity(vaultUri));
 
          return builder;
@@ -31,10 +36,48 @@ namespace Config.Net
       public static ConfigurationBuilder<TInterface> UseAzureKeyVaultWithServicePrincipal<TInterface>(
          this ConfigurationBuilder<TInterface> builder, Uri vaultUri, string clientId, string clientSecret) where TInterface : class
       {
+         if (vaultUri == null)
+         {
+            throw new ArgumentNullException(nameof(vaultUri));
+         }
+
+         if (clientId == null)
+         {
+            throw new ArgumentNullException(nameof(clientId));
+         }
+
+         if (clientSecret == null)
+         {
+            throw new ArgumentNullException(nameof(clientSecret));
+         }
+
          builder.UseConfigStore(AzureKeyVaultConfigStore.CreateWithPrincipal(vaultUri, clientId, clientSecret));
 
          return builder;
       }
 
+      /// <summary>
+      /// Use Azure Key Vault by authenticating either with service principal or managed identity. If clientId and clientSecret are null, managed
+      /// identity is used
+      /// </summary>
+      public static ConfigurationBuilder<TInterface> UseAzureKeyVaultWithServicePrincipalOrManagedIdentity<TInterface>(
+         this ConfigurationBuilder<TInterface> builder, Uri vaultUri, string clientId, string clientSecret) where TInterface : class
+      {
+         if (vaultUri == null)
+         {
+            throw new ArgumentNullException(nameof(vaultUri));
+         }
+
+         if (clientId == null && clientSecret == null)
+         {
+            builder.UseConfigStore(AzureKeyVaultConfigStore.CreateWithManagedIdentity(vaultUri));
+         }
+         else
+         {
+            builder.UseConfigStore(AzureKeyVaultConfigStore.CreateWithPrincipal(vaultUri, clientId, clientSecret));
+         }
+
+         return builder;
+      }
    }
 }
