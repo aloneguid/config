f7c5b6a66065d51f9366db7b0b7e2b5f25e7fff0
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2019-04-23 07:23:41 +0100

    using gitversion mainline mode

---
+++ src/Config.Net/Stores/JsonConfigStore.cs
@@ -0,0 +1,17 @@
+﻿#if CORE3
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Config.Net.Stores
+{
+   class JsonConfigStore
+   {
+      public JsonConfigStore()
+      {
+         JsonDocument doc = JsonDocument.Parse("");
+      }
+   }
+}
+#endif
