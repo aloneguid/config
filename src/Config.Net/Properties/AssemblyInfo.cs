d6d6ac04f61579c0fb8d260b9f3ec7a74949ae12
Ivan G <ivan.gavryliuk@outlook.com>
2019-11-28 15:45:11 +0000

    modernising, removing fake strong naming

--- src/Config.Net/Properties/AssemblyInfo.cs
+++ src/Config.Net/Properties/AssemblyInfo.cs
@@ -1,20 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Isoline Ltd")]
-[assembly: AssemblyProduct("Config.Net")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c71e3744-1544-47af-9a7b-e88f6f775975")]
-[assembly: InternalsVisibleTo("Config.Net.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001008f52c862bf5cd734565cc253e12b17b7779cb73297def09ec2a8215c2ebc11f575c847d50c2f30e7300cca469735c184b88ad7e501e0427cab29825c4389a51edb3e55cbb40c66c4e69c36c00db27af946980c6f854884e771ef4d92aea3f855998404d2f560417169068ab6f1052efcbf3219c266b6f953874c05050abe9bc2")]
+[assembly: InternalsVisibleTo("Config.Net.Tests")]
