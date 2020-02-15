1f9cb4a15c8857b256da2cfb6ac47fb3a8ded346
koryphaee <37508469+koryphaee@users.noreply.github.com>
2020-02-15 23:44:00 +0100

    added the Entity Framework Core configuration store

---
+++ src/Config.Net.EntityFrameworkCore/EntityFrameworkCoreConfigurationExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq.Expressions;
+using Config.Net.EntityFrameworkCore;
+using Config.Net.EntityFrameworkCore.Stores;
+using Microsoft.EntityFrameworkCore;
+
+namespace Config.Net
+{
+   /// <summary>
+   /// Configuration extensions
+   /// </summary>
+   public static class EntityFrameworkCoreConfigurationExtensions
+   {
+      /// <summary>
+      /// Uses Entity Framework Core as a builder storage.
+      /// This method allows you to use a custom configuration entity.
+      /// </summary>
+      /// <typeparam name="TContext">The type of DbContext to use.</typeparam>
+      /// <typeparam name="TEntity">The type of your custom entity</typeparam>
+      /// <param name="builder">Configuration object.</param>
+      /// <param name="context">The DbContext that will be used.</param>
+      /// <param name="predicateBuilder">Builds the query for EF Core.</param>
+      /// <param name="valueGetter">The getter for the config value.</param>
+      /// <param name="valueSetter">The setter for the config value.</param>
+      /// <param name="keySetter">The setter for the config key.</param>
+      /// <returns></returns>
+      public static ConfigurationBuilder<TInterface> UseEntityFrameworkCore<TInterface, TContext, TEntity>(
+         this ConfigurationBuilder<TInterface> builder,
+         TContext context,
+         Func<string, Expression<Func<TEntity, bool>>> predicateBuilder,
+         Func<TEntity, string> valueGetter,
+         Action<TEntity, string> valueSetter,
+         Action<TEntity, string> keySetter)
+         where TInterface : class
+         where TContext : DbContext
+         where TEntity : class, new()
+      {
+         IConfigStore configStore = new EntityFrameworkCoreConfigStore<TContext, TEntity>(
+            context,
+            predicateBuilder,
+            valueGetter,
+            valueSetter,
+            keySetter);
+         return builder.UseConfigStore(configStore);
+      }
+
+      /// <summary>
+      /// Uses Entity Framework Core as a builder storage.
+      /// To use this you have to declare a DbSet&lt;Setting&gt; in your DbContext.
+      /// </summary>
+      /// <typeparam name="TContext">The type of DbContext to use.</typeparam>
+      /// <param name="builder">Configuration object.</param>
+      /// <param name="context">The DbContext that will be used.</param>
+      /// <returns>Changed builder.</returns>
+      public static ConfigurationBuilder<TInterface> UseEntityFrameworkCore<TInterface, TContext>(
+         this ConfigurationBuilder<TInterface> builder,
+         TContext context)
+         where TInterface : class
+         where TContext : DbContext
+      {
+         IConfigStore configStore = new EntityFrameworkCoreConfigStore<TContext, Setting>(
+            context,
+            key => setting => setting.Key == key,
+            setting => setting.Value,
+            (setting, value) => setting.Value = value,
+            (setting, key) => setting.Key = key);
+         return builder.UseConfigStore(configStore);
+      }
+   }
+}
---
+++ src/Config.Net.Tests/Stores/EntityFrameworkCore/EntityFrameworkCoreConfigStoreTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Config.Net.EntityFrameworkCore;
+using Config.Net.EntityFrameworkCore.Stores;
+using Xunit;
+
+namespace Config.Net.Tests.Stores.EntityFrameworkCore
+{
+   public class EntityFrameworkCoreConfigStoreTests : AbstractTestFixture, IDisposable
+   {
+      private readonly string _currentFile;
+      private readonly EntityFrameworkCoreConfigStore<TestContext, Setting> _store;
+
+      public EntityFrameworkCoreConfigStoreTests()
+      {
+         string sample = Path.Combine(BuildDir.FullName, "TestData", "sample.db");
+         _currentFile = Path.Combine(BuildDir.FullName, "TestData", "current.db");
+         File.Copy(sample, _currentFile, true);
+         _store = new EntityFrameworkCoreConfigStore<TestContext, Setting>(
+            new TestContext(_currentFile),
+            key => setting => setting.Key == key,
+            setting => setting.Value,
+            (setting, value) => setting.Value = value,
+            (setting, key) => setting.Key = key);
+         _store.Write("somekey", "bla");
+      }
+
+      [Fact]
+      public void Read_existing()
+      {
+         string value = _store.Read("some_key");
+         Assert.NotNull(value);
+      }
+
+      [Fact]
+      public void Read_unknown()
+      {
+         string value = _store.Read("unknown_key");
+         Assert.Null(value);
+      }
+
+      [Fact]
+      public void Write_create_new()
+      {
+          _store.Write("new_key", "123");
+         Assert.Equal("123", _store.Read("new_key"));
+      }
+
+      [Fact]
+      public void Write_edit_existing()
+      {
+         _store.Write("some_key", "123");
+         Assert.Equal("123", _store.Read("some_key"));
+      }
+
+      public void Dispose()
+      {
+         _store.Dispose();
+         File.Delete(_currentFile);
+      }
+   }
+}
