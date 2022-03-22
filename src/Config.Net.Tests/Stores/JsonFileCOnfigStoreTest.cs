7b6538a6cd074f6f6abec886f3146cbff34a1e4d
DSikes <david@sikespc.com>
2022-03-22 08:59:52 -0400

    Corrected capitalization error

--- src/Config.Net.Tests/Stores/JsonFileCOnfigStoreTest.cs
+++ src/Config.Net.Tests/Stores/JsonFileCOnfigStoreTest.cs
@@ -8,12 +8,12 @@ using Xunit;
 
 namespace Config.Net.Tests.Stores
 {
-   public class JsonFileCOnfigStoreTest : AbstractTestFixture, IDisposable
+   public class JsonFileConfigStoreTest : AbstractTestFixture, IDisposable
    {
       private string _path;
       private JsonFileConfigStore _store;
 
-      public JsonFileCOnfigStoreTest()
+      public JsonFileConfigStoreTest()
       {
          _path = Path.Combine(BuildDir.FullName, "TestData", "sample.json");
          _store = new JsonFileConfigStore(_path, true);
