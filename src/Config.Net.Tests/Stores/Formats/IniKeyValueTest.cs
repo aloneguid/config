1ccac7c9e48a776afa118a4d44ba5335a598fc83
Matthias Kersting <aldart@gmail.com>
2022-09-07 11:43:09 +0200

    Check comment values in IniKeyValueTest

--- src/Config.Net.Tests/Stores/Formats/IniKeyValueTest.cs
+++ src/Config.Net.Tests/Stores/Formats/IniKeyValueTest.cs
@@ -6,17 +6,27 @@ namespace Config.Net.Tests.Stores.Formats
    public class IniKeyValueTest
    {
       [Theory]
-      [InlineData("key=value", "key", "value")]
-      [InlineData("key=value;123", "key", "value")]
-      [InlineData("key==value", "key", "=value")]
-      [InlineData("key=value;value;value", "key", "value;value")]
-      [InlineData("key=value=value;value", "key", "value=value")]
-      public void FromLine_ParsingInlineComments(string input, string expectedKey, string expectedValue)
+      [InlineData("key=value", "key", "value", null)]
+      [InlineData("key=value;123", "key", "value", "123")]
+      [InlineData("key==value", "key", "=value", null)]
+      [InlineData("key=value;value;value", "key", "value;value", "value")]
+      [InlineData("key=value=value;value", "key", "value=value", "value")]
+      [InlineData("key=value;rest;", "key", "value;rest", "")]
+      public void FromLine_ParsingInlineComments(string input, string expectedKey, string expectedValue, string expectedComment)
       {
          IniKeyValue kv = IniKeyValue.FromLine(input, true);
 
          Assert.Equal(expectedKey, kv.Key);
          Assert.Equal(expectedValue, kv.Value);
+         
+         if (expectedComment == null)
+         {
+            Assert.Null(kv.Comment);
+         }
+         else
+         {
+            Assert.Equal(expectedComment, kv.Comment.Value);
+         }
       }
 
       [Theory]
@@ -31,7 +41,7 @@ namespace Config.Net.Tests.Stores.Formats
 
          Assert.Equal(expectedKey, kv.Key);
          Assert.Equal(expectedValue, kv.Value);
+         Assert.Null(kv.Comment);
       }
-
    }
 }
