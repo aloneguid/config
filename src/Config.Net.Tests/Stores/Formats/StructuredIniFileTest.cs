81cf5444c8e19142c83e39687f3ce847088da252
Matthias Kersting <aldart@gmail.com>
2022-09-07 22:31:03 +0200

    Add escaping of new line characters for inline comments.

---
+++ src/Config.Net.Tests/Stores/Formats/StructuredIniFileTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Config.Net.Stores.Formats.Ini;
+using Xunit;
+
+namespace Config.Net.Tests.Stores.Formats
+{
+   public class StructuredIniFileTest : AbstractTestFixture
+   {
+      [Fact]
+      public void WriteInlineCommentWithNewLine()
+      {
+         string fullPath = Path.Combine(TestDir.FullName, Guid.NewGuid() + ".ini");
+         string content = @"key=value ;c1\r\nc2
+";
+
+         StructuredIniFile file;
+         using (Stream input = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+         {
+            file = StructuredIniFile.ReadFrom(input, true, true);
+         }
+
+         using (Stream output = File.OpenWrite(fullPath))
+         {
+            file.WriteTo(output);
+         }
+
+         string resultText = File.ReadAllText(fullPath);
+         Assert.Equal(content, resultText);
+      }
+   }
+}
